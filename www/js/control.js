4f32d3891e6a6812b93869121975ce62c887377d
John Alexander Torres <jalexandert@gmail.com>
2011-11-28 17:32:02 -0500

    Animated Href

--- www/js/control.js
+++ www/js/control.js
@@ -1,1 +1,23 @@
 // JavaScript Document
+var onAnterior;
+$(document).on("ready", arranque);
+function arranque()
+{
+	 $("#main-nav > li").on("click",onSelection);
+	 onAnterior= $("#main-nav ").children(0);
+}
+function goToByScroll(id)
+{
+  	$('html,body').animate({scrollTop: $("#"+id).offset().top},'fast');
+}
+function onSelection()
+{
+	if(onAnterior!=this)
+	{
+		
+		$(onAnterior).removeClass("active");
+		$(this).addClass("active");
+		//onAnterior=this;
+		//alert($(onAnterior).attr("class"));
+	}
+}
